ab35909907895adc6a73dfb2a8cf69edd0577c79
xiaomagua <1170556710@qq.com>
2022-10-16 09:42:05 +0800

    add first release

---
+++ RHHSSecure/Controller/Login.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  RHHSSecure
+//
+//  Created by chongchong ding on 10/15/22.
+//
+
+import UIKit
+
+class Login: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    //@IBOutlet weak var LoginEmail: UITextField!
+    
+    @IBOutlet weak var pwdField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func signinTapped(_ sender: Any) {
+        if let email = emailField.text, let pwd = pwdField.text{
+            if email == "eric@google.com" {
+                
+            }
+        }
+    }
+}
+
---
+++ RHHSSecure/Util/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  RHHSSecure
+//
+//  Created by chongchong ding on 10/16/22.
+//
+
+import UIKit
+
+let SHADOW_GRAY:CGFloat = 120.0/255.0
+
